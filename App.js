4ac2f92f27de3cb678c7f89e3f403521d2ab2695
sebasoba <76491101+sebasoba@users.noreply.github.com>
2021-07-13 18:31:41 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react=native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import WriteStoryScreen from './screens/WriteStoryScreen';
+import ReadStoryScreen from './screens/ReadStoryScreen';
